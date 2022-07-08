a20c1f0ed23c7d3965f03526ef738aaae913e7ff
Akame <Akame@localhost>
2022-07-08 07:44:52 +0200

    fichier de section de la partie back du blog

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+require('../includ/pdo.php');
+require('../includ/function.php');
+require('../includ/request.php');
+
+include('includ/header-back.php'); ?>
+
+<h1>Dashboard</h1>
+<?php
+include('includ/footer-back.php'); ?>
