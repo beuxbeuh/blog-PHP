b957f956759f40635c847e61582631a81f70a2ea
Akame <Akame@localhost>
2022-07-08 07:39:27 +0200

    fichier de section du blog en blog

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require('includ/pdo.php');
+require('includ/function.php');
+require('includ/request.php');
+
+
+include('includ/header.php'); ?>
+
+<h1>Home page</h1>
+
+<?php include('includ/footer.php'); ?>
